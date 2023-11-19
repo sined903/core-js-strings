8564e337ba8632dab6173a4ef7cad9b758fd29e8
Denis Kulikov <9933denis@gmail.com>
2023-11-19 12:53:52 +0300

    feat: add solution of extractNameFromTemplate

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -473,8 +473,11 @@ function getStringFromTemplate(firstName, lastName) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  const strToArr = value.split(' ');
+  strToArr.shift();
+  const joinArr = strToArr.join(' ');
+  return joinArr.slice(0, -1);
 }
 
 /**
